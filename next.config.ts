195bd089adc3ba2573a13f58520218f09d588cf3
Sarwar Hossain <sarwarhridoy4@gmail.com>
2024-11-17 14:03:54 +0600

    images.remotePatterns used

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
+    remotePatterns: [
       "github-readme-streak-stats.herokuapp.com",
       "github-readme-stats.vercel.app",
       "res.cloudinary.com",
